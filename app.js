a70fd93799e0dfb8460603d8a18ab8b3c668f396
yuuuri <yuuuri@yahoo.com>
2015-02-27 20:16:50 -0800

    new comment appears in comment section part is added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var main = function () {
     "use strict";
     
     $(".comment-input button").on("click", function (event) {
-        console.log("Hello World!");
+        $(".comments").append("<p>this is a new comment</p>");
     });
 };
 
