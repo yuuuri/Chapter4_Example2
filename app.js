e0a601b059557585ebafdef85a3b2d30294332f0
yuuuri <yuuuri@yahoo.com>
2015-02-27 23:40:49 -0800

    clearing out the input box

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ var main = function () {
     "use strict";
     
     $(".comment-input button").on("click", function (event) {
-        var $new_comment = $("<p>"),
-            comment_text = $(".comment-input input").val();
+        var $new_comment;
 
-        $new_comment.text(comment_text);
-
+        if ($(".comment-input input").val() !=="") {
+            $new_comment = $("<p>").text($(".comment-input input").val());
             $(".comments").append($new_comment);
+            $(".comment-input input").val("");
     
-    });
-};
+    }
+});
 
 $(document).ready(main);
