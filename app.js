6c2e3526438b552b26094c81f020640bfdc5499d
yuuuri <yuuuri@yahoo.com>
2015-02-27 22:37:53 -0800

    added val function so that input box of the text value of new paragraph element will be added to the comment on left

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ var main = function () {
     "use strict";
     
     $(".comment-input button").on("click", function (event) {
-        $(".comments").append("<p>this is a new comment</p>");
+        var $new_comment = $("<p>"),
+            comment_text = $(".comment-input input").val();
+
+        $new_comment.text(comment_text);
+
+            $(".comments").append($new_comment);
+    
     });
 };
 
