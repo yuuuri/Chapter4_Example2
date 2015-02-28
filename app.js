57ed581b8ef27809426decb8a7e9ca1ed4d433e1
yuuuri <yuuuri@yahoo.com>
2015-02-28 00:28:17 -0800

    keyCode is added

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 var main = function () {
     "use strict";
-    
     $(".comment-input button").on("click", function (event) {
         var $new_comment;
 
-        if ($(".comment-input input").val() !=="") {
-            $new_comment = $("<p>").text($(".comment-input input").val());
+        if  ($(".comment-input input").val() !== "") {
+            $new_comment = $("<p>").tet($(".comment-input input").val());
+            $new_comment.hide();
             $(".comments").append($new_comment);
+            $new_comment.fadeIn();
             $(".comment-input input").val("");
-    
-    }
-});
+        }
+    });
+
+    $("comment-input input").on("keypress", function (event) {
+        var $new_comment;
+
+        if (event.keyCode === 13) {
+            if ($(".comment-input input").val() !== "") {
+                var $new_comment = $("<p>").text($(".comment-input input").val());
+                $(".comments").append($new_comment);
+                $new_comment.fadeIn();
+                $(".comments").append($new_comment);
+                $(".comment-input input").val("");
+            }
+        }
+    });
+};
 
 $(document).ready(main);
